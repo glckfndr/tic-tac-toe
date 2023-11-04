4ca21c10ee16801157c8a9effe229fdea6672ac9
glckfndr <obulan65@gmail.com>
2023-11-04 20:25:52 +0200

    feat: Add selector of game type

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,14 @@
 require_relative 'tic_tac_toe/game'
 
-game = Game.new method(:puts)
+game_type = ''
+loop do
+  print 'Do you want to play with computer?(y/n): '
+  game_type = gets.chomp.downcase
+  break if %w[y n].include?(game_type)
+end
+game = if game_type == 'y'
+         Game.new method(:puts), true
+       else
+         Game.new method(:puts)
+       end
 game.play
